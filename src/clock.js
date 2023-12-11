68a74abe518f38b674b5677a74f8afa208e808b2
jangbly62 <82589477+jangbly62@users.noreply.github.com>
2023-12-11 12:20:36 +0900

    snowFlake Duration change
    0. 초 별로 눈송이 내림(초기 초에 해당 눈송이 만큼 한 번에 내림)
    1. 00초에 눈송이 다 녹음
    2. 낮 12시에 1초에 1번, 24시에 1초에 5송이 내림(1시간 간격으로 차이)

--- src/clock.js
+++ src/clock.js
@@ -1,11 +1,13 @@
 const body = document.querySelector("body");
 const clockDiv = document.querySelector("#clock");
 const clockSpan = document.querySelector("#clock span");
-const CLOCKPLACE = 650;
-const snowDuration = 15;
-const snowFrequency = 2;
-let dynamicStyles = null;
+const CLOCKPLACE = 800;
+const snowDuration = 60000;
 
+let dayHour = new Date().getHours();
+let snowFrequency = 1000 - (800 / 12) * Math.abs(dayHour - 12);
+let dynamicStyles = null;
+console.log(dayHour, snowFrequency);
 clockDiv.style.width = `${CLOCKPLACE}px`;
 
 function clockInterval() {
@@ -16,7 +18,7 @@ function clockInterval() {
   clockSpan.innerText = `${hour} : ${minute}`;
 }
 clockInterval();
-setInterval(clockInterval, 1000);
+setInterval(clockInterval, snowFrequency);
 
 function addAnimation(body) {
   if (!dynamicStyles) {
@@ -37,8 +39,7 @@ function clockSnowing() {
         snowDuration /
         180
     ) * 180;
-  const snowRange = -20 + (randomTime - 0.5) * 60;
-  console.log(snowRotate, snowRange);
+  const snowRange = -20 + (randomTime - 0.5) * 70;
   snow.classList = "fa-regular fa-snowflake";
   snow.style.color = `rgba(255,255,255,${0.5 + Math.random() * 0.5})`;
   snow.style.textShadow = "0 1px 10px rgba(0,0,0,0.3)";
@@ -70,22 +71,36 @@ function clockSnowing() {
         transform: translateY(250px) rotateY(${snowRotate}deg);}
     }`);
   snow.style.animation = `snowing ${
-    (randomTime * snowDuration) / 2 + snowDuration / 2
+    ((randomTime * snowDuration) / 2 + snowDuration / 2) / 2000
   }s ease-in-out forwards`;
   clockDiv.appendChild(snow);
 }
-function deleteSnow() {
-  const snowMelten = document.querySelector("#clock i:nth-child(2)");
-  console.log(snowMelten);
-  snowMelten.remove();
-  setTimeout(deleteSnow, snowFrequency * 100);
-}
-clockSnowing();
-console.log(clockDiv.childNodes[2]);
-setInterval(clockSnowing, snowFrequency * 100);
-setTimeout(deleteSnow, snowDuration * 4000);
 
-// let timer2 = setTimeout(function knock() {
-//   console.log("똑똑똑");
-//   timerId = setTimeout(knock, 1000);
-// }, 7000);
+function clockSnowingDefault(seconds) {
+  let defaultSeconds = 0;
+  while (defaultSeconds < (1000 / snowFrequency) * seconds) {
+    clockSnowing();
+    defaultSeconds += 1;
+  }
+}
+
+// function deleteSnow() {
+//   const snowMelten = document.querySelector("#clock i:nth-child(2)");
+//   console.log(snowMelten);
+//   snowMelten.remove();
+//   setTimeout(deleteSnow, snowFrequency);
+// }
+function deleteAllSnow() {
+  const snowRemain = document.querySelectorAll("#clock i");
+  const date = new Date();
+  if (date.getSeconds() === 0) {
+    for (let i = 0; i < snowRemain.length; i++) {
+      snowRemain[i].remove();
+    }
+  }
+}
+
+clockSnowingDefault(new Date().getSeconds());
+setInterval(clockSnowing, snowFrequency);
+// setTimeout(deleteSnow, snowDuration);
+setInterval(deleteAllSnow, snowFrequency);
